521f926ce37f2dfc3b02a998d332402923367059
Ronald Theodoro <ronaldtheodoro@gmail.com>
2021-09-26 17:53:02 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import json
+
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    with open('static/banlist.json', 'r') as fp:
+        payload = json.load(fp)
+    return jsonify(payload)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
